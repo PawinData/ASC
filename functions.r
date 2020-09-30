6268683aa4745d9b2f0542f262de0906988684c0
Paw in Data <59799041+PawinData@users.noreply.github.com>
2020-09-30 21:06:00 +0200

    self-defined functions

--- functions.r
+++ functions.r
@@ -36,3 +36,56 @@ root_NR <- function(g, init, eps=10^(-6))
 	}
 	return(list('root'=xx, 'num_iter'=count))
 }
+
+# fit X to a Gaussian Mixture Model defined by
+# component weights, component means, and component sd
+
+EM_init <- function(X,w_init,mu_init,sigma_init,eps=10^(-6))   # initialize by prior knowledge
+{
+  theta_old <- 0
+  theta_new <- c(w_init, mu_init, sigma_init)
+  
+  while (sum((theta_new-theta_old)^2) >= eps)
+  {
+    theta_old <- theta_new
+    # construct matrix Omega
+    T <- matrix(theta_old, ncol=3)
+    OMG <- c()
+    for (i in 1:length(X))
+    {
+      omg <- T[,1]*dnorm(X[i], mean=T[,2], sd=T[,3])
+      OMG <- c(OMG, omg/sum(omg))
+    }
+    OMG <- t(matrix(OMG, ncol=length(X)))
+    # update
+    w <- colSums(OMG)
+    w <- w/sum(w)
+    mu <- as.vector(X%*%OMG / colSums(OMG))
+    sig <- diag(t(outer(X,mu,`-`)^2) %*% OMG) / colSums(OMG)
+    theta_new <- c(w,mu,sqrt(as.vector(sig)))
+  }
+  
+  # output parameters as a matrix
+  return(matrix(theta_new, ncol=3))
+}
+
+EM_diff <- function(X,K)  # initialize by overall averge and sd of sample
+{
+  m <- mean(X)
+  s <- sd(X)
+  EM_init(X,
+          w_init = rep(1/K, K),
+          mu_init = seq(m-0.5*s*(K-1), m+0.5*s*(K-1), length.out=K),
+          sigma_init = rep(s,K)
+  )
+}
+
+EM_group <- function(X,K) # intialize by K-means group
+{
+  clusters <- kmeans(X,K)$cluster
+  EM_init(X, 
+          w_init = table(clusters)/length(X),
+          mu_init = tapply(X, clusters, mean),
+          sigma_init = tapply(X, clusters, sd)
+  )
+}
